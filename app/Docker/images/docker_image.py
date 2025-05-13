4a8594b15f184fa3ca6cb1a69722905692992d73
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-13 17:47:37 +0100

    renames docker_img to docker_images

---
+++ app/Docker/images/docker_image.py
@@ -0,0 +1,12 @@
+import docker 
+# set up a client 
+
+clients = docker.DockerClient.from_env()
+
+class DockerImageManagementService():
+    
+    def __init__(self):
+        pass
+    
+    
+    
--- app/Docker/images/docker_img.py
+++
@@ -1,4 +0,0 @@
-import docker 
-# set up a client 
-
-clients = docker.DockerClient(base_url="unix://var/run/docker.sock")
