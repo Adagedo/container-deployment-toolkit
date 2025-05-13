0bdd62653fdf5d5c0a1267d974ff400955617232
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-13 19:55:52 +0100

    modified the docker_image file

--- app/Docker/images/docker_image.py
+++ app/Docker/images/docker_image.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 # set up a client 
 
-clients = docker.DockerClient.from_env()
+client = docker.DockerClient.from_env()
 
 class DockerImageManagementService():
     
@@ -13,26 +13,106 @@ class DockerImageManagementService():
         if path is None:
             return TypeError(f"file path is not specifies")
         try:
-            image = clients.images.build(path=path)
+            image = client.images.build(path=path)
             return image
         except BuildError as ee:
             return BuildError
         except APIError as ee:
             return ee
+    
+    def get_image(self, name:str):
+        try:
+            image = client.images.get(name=name)
+            return image
+        except APIError as ee:
+            return ee 
+    
+    def get_registry_data(self, name:str):
         
-    def build_with_queit_tag(quiet:dict):
+        try:
+            data = client.images.get_registry_data(name=name)
+            return data
+        except APIError as ee:
+            return ee
+    def list_images(self, repository_name:str):
+        try:
+            images = client.images.list(name=repository_name)
+            return images
+        except APIError as ee:
+            return ee
+    def load_image_previously_saved(self, data:any) -> list[str|int]:
+        try:
+            images = client.images.load(data=data)
+            return images
+        except APIError as ee:
+            return ee
+        
+    def delete_unused_images(self)-> dict:
+        try:
+            response = client.images.prune(filters=None)
+            return response 
+        except APIError as ee:
+            return ee
+        
+    def build_with_queit_tag(self, quiet:dict):
         
         if quiet is None:
             return TypeError(f"file path is not specifies")
         try:
-            image = clients.images.build(quiet=quiet)
+            image = client.images.build(quiet=quiet)
             return image
-        except BuildError as ee:
-            return BuildError
         except APIError as ee:
             return ee
         
-        
+    def pull_images(self, repository:str):
+        try:
+            image = client.images.pull(repository=repository)
+            return image
+        except APIError as ee:
+            return ee
     
+    def pull_all_images(self, repository):
+        try:
+            images = client.images.pull(repository=repository, all_tags=True)
+            return images
+        except APIError as ee:
+            return ee
+    
+    
+    def push_image(self, repository:str):
+        try:
+            server_response = client.images.push(repository=repository)
+            return server_response
+        except APIError as ee:
+            return ee
+        
+    def remove(image:str):
+        try:
+            server_response = client.images.remove(image)
+            return server_response
+        except APIError as ee:
+            return ee
+        
+    def remove_image_force(image:str):
+    
+        try:
+            server_response = client.images.remove(image, force=True)
+            return server_response
+        except APIError as ee:
+            return ee
+        
+    def remove_image_with_untagged_parents(image:str):
+        try:
+            server_response = client.images.remove(image=image, unprune=True)
+            return server_response
+        except APIError as ee:
+            return ee
+        
+    def search_image(term:str, limit:int):
+        try:
+            response = client.images.search(term=term, limit=limit)
+            return response 
+        except APIError as ee:
+            return ee
     
     
