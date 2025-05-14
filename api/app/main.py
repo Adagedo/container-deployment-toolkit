e70e18b88b1c807ad82a51e4402dd52d5d381c1f
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-14 20:17:28 +0100

    created the api folder for the fastapi and created a docker compose file for further configuration

---
+++ api/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"msg":"DOJO"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
