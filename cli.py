8c0f9c45b09564720294f06f1749ec54b3ebb8bf
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-16 20:07:39 +0100

    this changes is not really usefull for now

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 # cli.py project entry
 import click
+import crud
 
 
 @click.group()
@@ -10,3 +11,8 @@ def cli():
 @cli.command()
 def createprovider():
     pass
+cli.add_command(crud.show)
+cli.add_command(crud.findme)
+cli.add_command(crud.commit)
+cli.add_command(crud.run)
+cli.add_command(crud.info)
