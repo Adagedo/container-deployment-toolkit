5428772a16b244425b346f10c8174859250d998b
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-26 17:16:54 +0100

    rename docker_auhthentication to docker_client_authentication

--- Orchestration_Plugins/Docker/docker_auth.py
+++ Orchestration_Plugins/Docker/docker_auth.py
@@ -2,7 +2,7 @@ from docker import DockerClient
 from docker.errors import DockerException, APIError
 
 
-def docker_authentication(username:str, password:str, registry:str="https://index.docker.io/v1/") -> DockerClient:
+def docker_client_authentication(username:str, password:str, registry:str="https://index.docker.io/v1/") -> DockerClient:
     try:
         client = DockerClient.from_env()
         response = client.login(username=username, password=password, registry=registry)
