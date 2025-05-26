80e4297ec8f5a96a88ed8287c1c0bd991450e07d
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-26 17:55:02 +0100

    updated the docker node file

--- Orchestration_Plugins/Docker/Nodes/docker_nodes.py
+++ Orchestration_Plugins/Docker/Nodes/docker_nodes.py
@@ -3,7 +3,7 @@ from docker import DockerClient
 from docker.errors import APIError
 from typing import Optional
 
-client = DockerClient.from_env()
+
 
 class Node():
     pass 
@@ -13,17 +13,21 @@ class Node():
 
 class DockerNodeManagementSystem():
     
+    
+    def __init__(self):
+        self.client = DockerClient.from_env()
+    
     def get_node_by_id_or_name(self, name:Optional[str]=None, id:Optional[str]=None):
         
         try:
-            node = client.nodes.get(node_id=name|id)
+            node = self.client.nodes.get(node_id=name|id)
             return node 
         except APIError as ee:
             return ee
         
     def list_node(self, filter:Optional[dict[str]])-> list[Node]:
         try:
-            list_of_nodes = client.nodes.list(filters=filter)
+            list_of_nodes = self.client.nodes.list(filters=filter)
             return list_of_nodes
         except APIError as ee:
             return ee
