6f2f976a54ca53a81347d17779ec03b1891688fb
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-05-26 19:23:53 +0100

    updated user data into a secrete config file

--- Orchestration_Plugins/Docker/docker_auth.py
+++ Orchestration_Plugins/Docker/docker_auth.py
@@ -1,12 +1,14 @@
 from docker import DockerClient
 from docker.errors import DockerException, APIError
+from typing import Optional
+from config.docker.docker_config import update_config
 
-
-def docker_client_authentication(username:str, password:str, registry:str="https://index.docker.io/v1/") -> DockerClient:
+def docker_client_authentication(username:str, password:str, registry:str="https://index.docker.io/v1/") -> Optional[DockerClient]:
     try:
         client = DockerClient.from_env()
         response = client.login(username=username, password=password, registry=registry)
         print(f"login successfull:{response.get('Username')}")
+        update_config(config_data=response)
         return client if response else None 
     except APIError as ee:
         print(f"docker login failed:{ee.explanation}")
@@ -14,5 +16,8 @@ def docker_client_authentication(username:str, password:str, registry:str="https
         print(f"docker error:{ee}")
     except Exception as e:
         print(f"unexpected error during login:{e}")
-    
 
+def isAuthenticated()->bool:
+    pass
+    
+    
